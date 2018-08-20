26bf2016e34516c8db3a5a9e927cf9edd331389b
Bruce Jia <jiafangtao@gmail.com>
2018-08-20 12:47:53 +0800

    Fix JS code style issues

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,43 +3,42 @@
 angular.module('worklogApp')
   .controller('MainController', ['$scope', function ($scope) {
   	
-	  $scope.text = "bingo!";
-	  $scope.newTaskText = "";
+	  $scope.newTaskText = '';
 
     $scope.tasks = {
     	
     	planned: [
 	    	{
 	    		id: 1,
-	    		title: "task 1"
+	    		title: 'task 1'
 	    	},
 	    	{
 	    		id: 2,
-	    		title: "task 2"
+	    		title: 'task 2'
 	    	},
 	    	{
 	    		id: 3,
-	    		title: "task 3 has a long name"
+	    		title: 'task 3 has a long name'
 	    	}
     	],
     	done: [
     		{
 	    		id: 100,
-	    		title: "I have done this."
+	    		title: 'I have done this.'
 	    	},
 	    	{
 	    		id: 101,
-	    		title: "I have done that."
+	    		title: 'I have done that.'
 	    	},
 	    	{
 	    		id: 102,
-	    		title: "I have done another one, with a long name"
+	    		title: '"I have done another one, with a long name'
 	    	}
     	]
 	};
 	
 	$scope.createNewTask = function () {
-		console.info ("Creating a new task ...");
+		console.info ('Creating a new task ...');
 		if (this.newTaskText) {
 			var id = this.tasks.planned[this.tasks.planned.length - 1].id + 1;
 			//var timestamp = '';
