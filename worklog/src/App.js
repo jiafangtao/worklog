d80c428bdb87b77e9d2bef4246b9fe09b0fbc1ad
Bruce Jia <bruce.jia@autodesk.com>
2022-02-18 19:29:06 +0800

    Trying to add a simple function
    Not working yet. The button doesn't function yet.

--- worklog/src/App.js
+++ worklog/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import addItem from './Item.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Simply log your work locally
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+
+      <div className="List">
+        <input className="ListButton" type="button" value="Add" onClick={addItem}/>
+      </div>
     </div>
   );
 }
---
+++ worklog/src/Item.js
@@ -0,0 +1,14 @@
+
+function Item() {
+  return (
+    <div className="Item">
+      here is an item
+    </div>
+  );
+}
+
+function addItem() {
+  console.log("Going to add an item ...");
+}
+
+export default { Item, addItem };
